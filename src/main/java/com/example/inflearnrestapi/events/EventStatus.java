b53e74444b38c992fcb7e5d1db9f004e62337ae0
조원진 <>
2020-11-09 20:29:06 +0900

    Event domain

---
+++ src/main/java/com/example/inflearnrestapi/events/EventStatus.java
@@ -0,0 +1,5 @@
+package com.example.inflearnrestapi.events;
+
+public enum EventStatus {
+    DRAFT, PUBLISHED, BEGAN_ENROLLMENT;
+}
---
+++ src/test/java/com/example/inflearnrestapi/events/EventTest.java
@@ -0,0 +1,32 @@
+package com.example.inflearnrestapi.events;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EventTest {
+
+    @Test
+    public void builder(){
+        Event event = Event.builder()
+                .name("Inflearn Spring REST API")
+                .description("REST API development with Spring")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean(){
+        Event event = new Event();
+        String name = "Event";
+        event.setName(name);
+        String spring = "Spring";
+        event.setDescription(spring);
+
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(spring);
+
+
+    }
+}
