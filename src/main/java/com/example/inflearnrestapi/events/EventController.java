60ce0fa20b9f91e70ea8158e2702a3030cd7febc
조원진 <>
2020-11-09 22:18:12 +0900

    POST 201 RETURN

---
+++ src/main/java/com/example/inflearnrestapi/events/EventController.java
@@ -0,0 +1,30 @@
+package com.example.inflearnrestapi.events;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
+@RequiredArgsConstructor
+public class EventController {
+
+    private final EventRepository eventRepository;
+
+    @PostMapping
+    public ResponseEntity createEvent(@RequestBody Event event){
+        Event newEvent = this.eventRepository.save(event);
+        URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
+        return ResponseEntity.created(createdUri).body(event);
+    }
+}
